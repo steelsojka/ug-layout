3f11771837bddbbaf16bc5ad2acddea624a49198
Steven Sojka <Steven.Sojka@tdameritrade.com>
2017-06-27 10:17:24 -0500

    feat(dom): export snabbdom utils

--- layout/src/dom/index.ts
+++ layout/src/dom/index.ts
@@ -5,3 +5,5 @@ export * from './Renderable';
 export * from './RenderableArea';
 export * from './decorators';
 export * from './common';
+export { h, thunk } from 'snabbdom';
+export { VNode, VNodeData, vnode } from 'snabbdom/vnode';
